3908603047b431637d85555bee340dd59c5fccc2
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-10-02 22:17:53 +0530

    fixed port

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { message });
 });
 
-app.listen(3000, () => {
-    console.log("App is listening on port 4000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("App is listening on port 3000");
 });
